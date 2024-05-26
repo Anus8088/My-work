1cdbdabf7a4b0f0b19946a8860f01b0767e9030d
Muhammad Anus <anusj8088@gmail.com>
2024-05-26 06:16:38 -0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,290 @@
+// Make up kit Functionality start
+function total_01() {
+    var a = document.getElementById('price_01').innerText;
+    var b = document.getElementById('qty_01').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+
+function total_02() {
+    var a = document.getElementById('price_02').innerText;
+    var b = document.getElementById('qty_02').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+
+function total_03() {
+    var a = document.getElementById('price_03').innerText;
+    var b = document.getElementById('qty_03').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+
+function total_04() {
+    var a = document.getElementById('price_04').innerText;
+    var b = document.getElementById('qty_04').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+
+function total_05() {
+    var a = document.getElementById('price_05').innerText;
+    var b = document.getElementById('qty_05').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+
+function total_06() {
+    var a = document.getElementById('price_06').innerText;
+    var b = document.getElementById('qty_06').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+
+// Make up kit Functionality end
+
+// Nail Paint Functionality start
+function total_07() {
+    var a = document.getElementById('price_07').innerText;
+    var b = document.getElementById('qty_07').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+
+function total_08() {
+    var a = document.getElementById('price_08').innerText;
+    var b = document.getElementById('qty_08').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_09() {
+    var a = document.getElementById('price_09').innerText;
+    var b = document.getElementById('qty_09').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_10() {
+    var a = document.getElementById('price_10').innerText;
+    var b = document.getElementById('qty_10').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_11() {
+    var a = document.getElementById('price_11').innerText;
+    var b = document.getElementById('qty_11').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_12() {
+    var a = document.getElementById('price_12').innerText;
+    var b = document.getElementById('qty_12').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+// Nail Paint Functionality end
+
+// Hair Oil Functionality start
+function total_13() {
+    var a = document.getElementById('price_13').innerText;
+    var b = document.getElementById('qty_13').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_14() {
+    var a = document.getElementById('price_14').innerText;
+    var b = document.getElementById('qty_14').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_15() {
+    var a = document.getElementById('price_15').innerText;
+    var b = document.getElementById('qty_15').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_16() {
+    var a = document.getElementById('price_16').innerText;
+    var b = document.getElementById('qty_16').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_17() {
+    var a = document.getElementById('price_17').innerText;
+    var b = document.getElementById('qty_17').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_18() {
+    var a = document.getElementById('price_18').innerText;
+    var b = document.getElementById('qty_18').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+// Hair Oil Functionality end
+
+// Face Wash Functionality start
+function total_19() {
+    var a = document.getElementById('price_19').innerText;
+    var b = document.getElementById('qty_19').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_20() {
+    var a = document.getElementById('price_20').innerText;
+    var b = document.getElementById('qty_20').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_21() {
+    var a = document.getElementById('price_21').innerText;
+    var b = document.getElementById('qty_21').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_22() {
+    var a = document.getElementById('price_22').innerText;
+    var b = document.getElementById('qty_22').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_23() {
+    var a = document.getElementById('price_23').innerText;
+    var b = document.getElementById('qty_23').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+function total_24() {
+    var a = document.getElementById('price_24').innerText;
+    var b = document.getElementById('qty_24').value;
+    var c = a * b;
+    if (a >= 0 && b >= 0) {
+        alert("YOur Total amount is :" + c);
+    }
+    else {
+        alert("Enter Correct Value")
+    }
+};
+// Face Wash Functionality end
+
+
+
+
+
+
+
+
